7901c122d8f03fdeb622059b60f4c6cd57d9fd7a
anhntth1807026 <anhntth1807026@fpt.edu.vn>
2019-03-23 21:10:14 +0700

    add test 3

--- src/controller/FoodController.java
+++ src/controller/FoodController.java
@@ -7,7 +7,8 @@ import java.util.Scanner;
 
 public class FoodController {
     private ArrayList<Food> list = new ArrayList<>();
-    public void addFood(){
+
+    public void addFood() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Vui lòng nhập mã món ăn: ");
         String ID = scanner.nextLine();
@@ -19,12 +20,27 @@ public class FoodController {
         String price = scanner.nextLine();
         System.out.println("Vui lòng nhập ngày tạo: ");
         String createAt = scanner.nextLine();
+        System.out.println("Vui lòng lựa chọn danh mục: ");
+        System.out.println("1. Đồ ăn.");
+        System.out.println("2. Đồ uống.");
+        int choice = scanner.nextInt();
+        switch (choice) {
+            case 1:
+
+                break;
+            case 2:
+                break;
+            default:
+                System.out.println("Lựa chọn sai. Vui lòng chọn 1 hoặc 2.");
+                break;
+        }
         Food food = new Food(ID, name, description, price, createAt);
         list.add(food);
     }
-    public void printList(){
+
+    public void printList() {
         System.out.printf("%-10s %-10s %-10s %-10s %-10s\n", "Mã", "Tên món", "Giá", "Mô tả", "Ngày phát hành");
-        for (int i = 0; i < list.size() ; i++) {
+        for (int i = 0; i < list.size(); i++) {
             Food food = list.get(i);
             System.out.println(food.toString());
         }
